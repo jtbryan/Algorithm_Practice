cd9ff021fe1b130f98d300f9f3004b4f1778bb5d
jtbryan <jonnib24@gmail.com>
2020-08-13 12:16:20 -0400

    Added merge sort for python

---
+++ Merge_Sort/merge.py
@@ -0,0 +1,45 @@
+import sys
+
+def mergeSort(items):
+    n = len(items)
+    if(n == 1):
+        return items
+    fhalfend = int(n/2)
+    shalfstart = int((n/2))
+    fhalf = items[:fhalfend]
+    shalf = items[shalfstart:]
+    fhalf = mergeSort(fhalf)
+    shalf = mergeSort(shalf)
+    return merge(fhalf, shalf)
+    
+def merge(list1, list2):
+    
+    newarr = []
+    while len(list1) > 0 and len(list2) > 0:
+        if list1[0] > list2[0]:
+            newarr.append(list2[0])
+            list2.pop(0)
+        else:
+            newarr.append(list1[0])
+            list1.pop(0)
+    while len(list1) > 0:
+        newarr.append(list1[0])
+        list1.pop(0)
+        
+    while len(list2) > 0:
+        newarr.append(list2[0])
+        list2.pop(0)
+    return newarr
+
+if __name__ == "__main__":
+
+    num_items = 0
+    items = []
+
+    input_items = input("Enter your values seperated by a space: ")
+
+    items = input_items.split()
+
+    items = [ int(x) for x in items ]
+    items = mergeSort(items)
+    print(items)
